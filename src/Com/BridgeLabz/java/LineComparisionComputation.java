c7882f8c85ebf26f400e62608211d6a761e14ab9
Avinash <aj912873@gmail.com>
2022-02-16 02:19:59 +0530

    Added:Welcome to Line Comparison Computation Program using OOPS

---
+++ src/Com/BridgeLabz/java/LineComparisionComputation.java
@@ -0,0 +1,8 @@
+package Com.BridgeLabz.java;
+
+public class LineComparisionComputation {
+	public static void main(String[]Args) {
+		System.out.println("Welcome to Line Comparison Computation Program using OOPS");
+	}
+
+}
